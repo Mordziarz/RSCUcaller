511c1acfc4e6801fb193a1c75b7b7aa78aeb3c1f
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2024-12-30 18:59:21 +0100

    Update boxplot_between_groups.R

--- R/boxplot_between_groups.R
+++ R/boxplot_between_groups.R
@@ -37,11 +37,11 @@ boxplot_between_groups <-function(get_RSCU_out,grouping_table,width,height,xlab,
     statistical_table <- base::data.frame(row.names = 1, .y. = NA, group1 = NA, group2 = NA, n1 = NA, n2 = NA, statistic = NA, p=NA, p.adj=NA, p.adj.signif=NA)
     get_RSCU_out <- merge(get_RSCU_out,grouping_table,by="Species")
     get_RSCU_out$index2 <- paste0(get_RSCU_out$group,"_",get_RSCU_out$codon)
+    get_RSCU_out$group <- get_RSCU_out[order(get_RSCU_out$group),]
     
     for (i in 1:base::length(codons)) {
       table_1 <- get_RSCU_out[get_RSCU_out$codon %in% codons[i],]
       table_1 <- table_1[,c("RSCU", "index2","group")]
-      table_1$group <- table_1[order(table_1$group),]
       stat <- stats::kruskal.test(RSCU ~ index2 , data = table_1)
       post_hoc <- base::as.data.frame(rstatix::dunn_test(RSCU ~ index2, data = table_1, p.adjust.method = "bonferroni"))
       statistical_table <- base::rbind(statistical_table,post_hoc)
